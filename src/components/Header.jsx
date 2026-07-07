4f1c40709fe831fccc9ceaaeddc01ad3ae512d5a
Honour Shalom <shalomhonour1@gmail.com>
2026-07-07 18:44:57 +0100

    corrections

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ import dollarsign from "../assets/dollar-sign.svg";
 function Header() {
     return(
         <>
-            <h1 className="text-[#7AAA8A] font-normal font-instrument leading-none tracking-normal"> <img src={dollarsign} alt="" className="w-2.5 h-4.5" />TIP CALCULATOR</h1>
-            <h1 className="text-[#FFFFFF] font-normal text-4xl font-georama, mt-2.25 leading-none tracking-normal">Split the bill,</h1>
-            <h1 className="text-[#FFFFFF] text-[64px] leading-none tracking-normal font-licorice font-normal mt-px">not the friendship.</h1>
+            <h1 className="flex items-center gap-[6px]"> <img src={dollarsign} alt="" className="w-2.5 h-4.5 border-[#0D2218] border-[2px] " /><span className="text-[#7AAA8A] font-normal font-instrument  text-[12px]">TIP CALCULATOR</span></h1>
+            <h1 className="text-[#FFFFFF] font-normal text-4xl font-georama, mt-2.25">Split the bill,</h1>
+            <h1 className="text-[#FFFFFF] text-[64px]  font-licorice font-normal mt-px">not the friendship.</h1>
         </>
     )
 }
